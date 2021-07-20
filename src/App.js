016cff4807da102c7182855f44dbcb0ae7ddb014
santu369 <janashivasantosh@gmail.com>
2021-07-20 16:57:34 +0530

    create-react-app, added template, added sass

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is react app with sass</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
